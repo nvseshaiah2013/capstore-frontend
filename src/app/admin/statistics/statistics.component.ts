c0822b770ddeb2169a193ec765581c28f4663482
nvseshaiah2013 <26201410+nvseshaiah2013@users.noreply.github.com>
2020-05-16 09:16:21 +0530

    Minor Bug Fix

--- src/app/admin/statistics/statistics.component.ts
+++ src/app/admin/statistics/statistics.component.ts
@@ -54,5 +54,5 @@ export class StatisticsComponent implements OnInit {
       })
     })
 
-
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { HttpClientModule } from '@angular/common/http';
     HomeComponent,
     NotFoundComponent,
     SearchCustomerPipe,
-    ServerErrorComponent
+    ServerErrorComponent,
 
     SearchCustomerPipe
 
