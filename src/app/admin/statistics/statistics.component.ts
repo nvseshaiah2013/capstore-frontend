d91a446a8f95d38ab6096a3a585a9336bd8f29b9
nvseshaiah2013 <26201410+nvseshaiah2013@users.noreply.github.com>
2020-05-16 10:00:06 +0530

    Statistics Chart Fix

--- src/app/admin/statistics/statistics.component.ts
+++ src/app/admin/statistics/statistics.component.ts
@@ -53,7 +53,53 @@ export class StatisticsComponent implements OnInit {
         }
       })
     })
+    this.chart = new Chart('revenueChart', {
+      type: 'line',
+      data: {
+        labels: this.date,
+        datasets: [
+          {
+            label: 'Total Revenue (in INR) - Last 7 Days',
+            backgroundColor: '#66bb6a',
+            borderColor: '#66bb6a',
+            data: this.totalRevenue,
+            fill: false,
+          }
+        ]
+      }
+    })
 
+    this.chart = new Chart('ordersChart', {
+      type: 'bar',
+      data: {
+        labels: this.date,
+        datasets: [
+          {
+            label: 'No of Orders - Last 7 Days',
+            backgroundColor: '#007bff',
+            borderColor: 'green',
+            data: this.noOfOrders,
+            fill: false,
+          }
+        ]
+      }
+    })
+
+    this.chart = new Chart('categoryChart', {
+      type: 'pie',
+      data: {
+        labels: this.category,
+        datasets: [
+          {
+            label: 'Category Wise Orders',
+            backgroundColor: ['#878BB6', '#4ACAB4', '#FF8153', '#FFEA88'],
+            borderColor: ['#878BB6', '#4ACAB4', '#FF8153', '#FFEA88'],
+            data: this.categoryOrders,
+            fill: true,
+          }
+        ]
+      }
+    })
 
 }
 }
