3199114f96a8d4c9970d622394ef50f2f49dfd13
Jatin <jatinmadaan9882@gmail.com>
2020-05-17 21:31:19 +0530

    Merchant can now view todays Total revenue in dashboard

--- src/app/merchant/merchant-home/merchant-home.component.ts
+++ src/app/merchant/merchant-home/merchant-home.component.ts
@@ -12,6 +12,8 @@ export class MerchantHomeComponent implements OnInit {
   activeCustomers : number;
   cancelledOrders : number = 0;
   usernameList = new Set<string>();
+  todayRevenue : number = 0;
+
   constructor(private merchantService : MerchantService) { }
   ngOnInit() {
     this.merchantService.getMerchantOrders('harsha98').subscribe(data => {
@@ -20,6 +22,11 @@ export class MerchantHomeComponent implements OnInit {
         this.usernameList.add(element.customer.username);
         if(element.orderStatus == "Cancelled"){
           this.cancelledOrders += 1;
+        }
+        
+        //console.log(new Date(element.transaction.transactionDate).toLocaleDateString() ==  new Date().toLocaleDateString());
+        if(new Date(element.transaction.transactionDate).toLocaleDateString() ==  new Date().toLocaleDateString()){
+          this.todayRevenue += element.transaction.transactionMoney;
         }
       });
       this.activeCustomers = this.usernameList.size;
