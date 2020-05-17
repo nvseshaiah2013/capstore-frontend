d1d608dd3d599d84872f87b99723d5360cdfbc4b
Pritam Kumar <officialpritam1105@gmail.com>
2020-05-17 14:00:01 +0530

    Inventory page

--- src/app/merchant/inventory/inventory.component.ts
+++ src/app/merchant/inventory/inventory.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventoryComponent implements OnInit {
 
+  view:boolean=true;
+  pp=[];
+  cc=[];
+  customers:any;
   constructor() { }
 
   ngOnInit() {
+    this.cc.push(1);
+    this.pp.push(1);
+    this.pp.push(1);
+    this.pp.push(2);
+    this.pp.push(3);
+    this.pp.push(4);
+    this.pp.push(2);
+    this.pp.push(3);
+    this.pp.push(4);
   }
+
+  deleteProduct()
+  {
+    console.log("Product Name");
+  }
+  viewDetails()
+  {
+    console.log("Product");
+  }
+
+  
 
 }
